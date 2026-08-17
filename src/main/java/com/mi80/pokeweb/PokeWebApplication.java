2d00f53a66f9a38f9c00eb9ad4dc80cfdae0eaff
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-08-17 15:57:26 -0300

    Initial commit

---
+++ src/main/java/com/mi80/pokeweb/PokeWebApplication.java
@@ -0,0 +1,13 @@
+package com.mi80.pokeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mi80/pokeweb/PokeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi80.pokeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
